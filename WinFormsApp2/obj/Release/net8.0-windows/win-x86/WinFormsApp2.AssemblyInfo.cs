4a8f72b75ebc7c36d4979f5f4a6fce7db4faed63
forestk1ne <128807992+rockysx27@users.noreply.github.com>
2025-05-13 04:07:00 +0100

    upd

--- WinFormsApp2/obj/Release/net8.0-windows/win-x86/WinFormsApp2.AssemblyInfo.cs
+++ WinFormsApp2/obj/Release/net8.0-windows/win-x86/WinFormsApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39dd4c842303ceaf0f22e9488e493d7428524c75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+689ea1f3f4640c14af43158bd972d26ce358f033")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
